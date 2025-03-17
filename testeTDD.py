4a2e7174dad48e0fef96be9424f6aaad5fc08a29
Beatriz <beatriz.bramont@gmail.com>
2025-03-17 18:50:07 -0300

    Temos 20 testes

--- testeTDD.py
+++ testeTDD.py
@@ -6,6 +6,50 @@ class TestStringMethods(unittest.TestCase):
 
 #Testar criação
 
+    def test_campo_professor_nome_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "nome": None,
+            "idade": 0,
+            "materia": "Nome da materia",
+            "observacoes": "Observacao sobre o professor"
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo nome informado é obrigatório.', r.json()['error'])
+
+    def test_campo_professor_idade_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "nome": "Priscilla",
+            "idade": None,
+            "materia": "Nome da materia",
+            "observacoes": "Observacao sobre o professor"
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo idade informado é obrigatório.', r.json()['error'])
+
+    def test_campo_professor_materia_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "nome": "Marcos",
+            "idade": 0,
+            "materia": None,
+            "observacoes": "Observacao sobre o professor"
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo materia informado é obrigatório.', r.json()['error'])
+
+    def test_campo_professor_obervacoes_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "nome": "Lima",
+            "idade": 0,
+            "materia": "Fisica",
+            "observacoes": None
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo observacoes informado é obrigatório.', r.json()['error'])
+
     def test_criar_professor(self):
         r = requests.post('http://127.0.0.1:5000/professor', json={
             'id':2,
@@ -59,6 +103,36 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(obj_retornado),type([]))   
 
 
+    def test_campo_turma_descricao_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "descricao": None,
+            "professor_id": 1,
+            "ativo": "Status"
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo descricao informado é obrigatório.', r.json()['error'])
+
+    def test_campo_turma_professor_id_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "descricao": "testes rapidos",
+            "professor_id": None,
+            "ativo": "Status"
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo professor_id informado é obrigatório.', r.json()['error'])
+
+    def test_campo_ativo_descricao_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            "id": '',
+            "descricao": "Probabilidades",
+            "professor_id": 1,
+            "ativo": None
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo ativo informado é obrigatório.', r.json()['error'])
+
     def test_criar_turma(self):
         r = requests.post('http://127.0.0.1:5000/turma', json={
             'id':2,
@@ -109,6 +183,104 @@ class TestStringMethods(unittest.TestCase):
         
         self.assertEqual(type(obj_retornado),type([]))
 
+    def test_campo_nome_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': None,
+            'idade': 20,
+            'data_nascimento': '01/01/2000',
+            'nota_primeiro_semestre': 8.0,
+            'nota_segundo_semestre': 9.0,
+            'media_final': 8.5,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo nome informado é obrigatório.', r.json()['error'])
+    
+    def test_campo_idade_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Manuela',
+            'idade': None,
+            'data_nascimento': '01/01/2000',
+            'nota_primeiro_semestre': 8.0,
+            'nota_segundo_semestre': 9.0,
+            'media_final': 8.5,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo idade informado é obrigatório.', r.json()['error'])
+
+    def test_campo_datanasc_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Angelina',
+            'idade': 50,
+            'data_nascimento': None,
+            'nota_primeiro_semestre': 8.0,
+            'nota_segundo_semestre': 9.0,
+            'media_final': 8.5,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo data_nascimento informado é obrigatório.', r.json()['error'])
+
+    def test_campo_nota_primeiro_semestre_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Beatriz',
+            'idade': 19,
+            'data_nascimento': '03/10/2005',
+            'nota_primeiro_semestre': None,
+            'nota_segundo_semestre': 9.0,
+            'media_final': 8.5,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo nota_primeiro_semestre informado é obrigatório.', r.json()['error'])
+
+    def test_campo_nota_segundo_semestre_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Bianca',
+            'idade': 19,
+            'data_nascimento': '03/10/2005',
+            'nota_primeiro_semestre': 10.0,
+            'nota_segundo_semestre': None,
+            'media_final': 8.5,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo nota_segundo_semestre informado é obrigatório.', r.json()['error'])
+
+    def test_campo_media_final_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Beatriz',
+            'idade': 19,
+            'data_nascimento': '03/10/2005',
+            'nota_primeiro_semestre': 8.0,
+            'nota_segundo_semestre': 9.0,
+            'media_final': None,
+            'turma_id': 1
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo media_final informado é obrigatório.', r.json()['error'])
+    
+    def test_campo_turma_id_null(self):
+        r = requests.post('http://127.0.0.1:5000/alunos', json={
+            'id': '',
+            'nome': 'Beatriz',
+            'idade': 19,
+            'data_nascimento': '03/10/2005',
+            'nota_primeiro_semestre': 5.0,
+            'nota_segundo_semestre': 9.0,
+            'media_final': 8.5,
+            'turma_id': None
+        })
+        self.assertEqual(r.status_code, 400)
+        self.assertIn('O campo turma_id informado é obrigatório.', r.json()['error'])
+
     def test_criar_aluno(self):
         r = requests.post('http://127.0.0.1:5000/alunos', json={
             'id': 4,
@@ -153,6 +325,7 @@ class TestStringMethods(unittest.TestCase):
             self.fail('Aluno José não apareceu na lista de alunos')
         if not achei_leticia:
             self.fail('Aluna Letícia não apareceu na lista de alunos')
+
     
     def test_retorna_lista_alunos(self):
         r = requests.get('http://127.0.0.1:5000/alunos')
@@ -167,6 +340,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(obj_retornado),type([]))
 
         
+    
+
+
+
+
 def runTests():
         suite = unittest.defaultTestLoader.loadTestsFromTestCase(TestStringMethods)
         unittest.TextTestRunner(verbosity=2,failfast=True).run(suite)
@@ -177,7 +355,7 @@ if __name__ == '__main__':
 
 
 
-# Testar campos nulos
+
 # Testar Atualizar
 # Testar Apagar
 # Testar reseta
